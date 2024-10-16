50f6eccba63e619c3197f094ad7a9181802bacfd
danieljuli2003 <danifelipe2003.16@gmail.com>
2024-10-16 17:19:09 -0500

    Add files via upload

---
+++ Proyect_puedes ser/index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sabia que ibas a decir que si como TE AMO MI AMOR')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function() {
+    alert('ERROR DE LA PAGINA INTENTALO NUEVAMENTE')
+});
